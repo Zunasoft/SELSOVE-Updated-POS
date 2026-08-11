5dfe7bbbd97c2b4242496d03add6c0f6701cd669
apple <apple@apples-MacBook-Pro.local>
2026-08-11 19:23:52 +0530

    new

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const ADMIN_BE = 'http://localhost:5001/api';
-// export const ADMIN_BE = 'selsolve-updated-backend.vercel.app/api'
+// export const ADMIN_BE = 'http://localhost:5001/api';
+export const ADMIN_BE = 'https://selsolve-updated-backend.vercel.app/api'
